3be0657cf8369bbd9a1e13eb95557dacb96d559c
Alan Fernandes <alanfernandes78@hotmail.com>
2022-06-15 01:26:11 -0300

    Pasta 'Compartilhado', classe ConexaoSql e Interface IRepositorio criadas.

---
+++ ControleDeMedicamentos.Infra.BancoDeDados/Compartilhado/ConexaoSql.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace ControleDeMedicamentos.Infra.BancoDeDados.Compartilhado
+{
+    public class ConexaoSql
+    {
+        protected string StringConexao = @"
+            Data Source=(LocalDB)\MSSqlLocalDB;
+            Initial Catalog=DBControleDeMedicamentos;   
+            Integrated Security=True;
+            Pooling=False";
+
+        protected SqlConnection? Conexao = null;
+    }
+}
---
+++ ControleDeMedicamentos.Infra.BancoDeDados/Compartilhado/IRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using ControleDeMedicamentos.Dominio.Compartilhado;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace ControleDeMedicamentos.Infra.BancoDeDados.Compartilhado
+{
+    public interface IRepositorio<T> where T : EntidadeBase<T>
+    {
+        ValidationResult Inserir(T t);
+        ValidationResult Editar(T t);
+        ValidationResult Excluir(T t);
+        List<T> SelecionarTodos();
+        T SelecionarPorId(int id);
+        AbstractValidator<T> ObterValidador();
+    }
+}
