2334bbed6e314f3a89173ebcf771ba8828c39610
Alan Fernandes <alanfernandes78@hotmail.com>
2022-06-16 15:44:57 -0300

    Implementadas as classes Requisicao e ValidadorRequisicao

---
+++ ControleDeMedicamentos.Dominio/ModuloRequisicao/Requisicao.cs
@@ -0,0 +1,32 @@
+﻿using ControleDeMedicamentos.Dominio.Compartilhado;
+using ControleDeMedicamentos.Dominio.ModuloFuncionario;
+using ControleDeMedicamentos.Dominio.ModuloMedicamento;
+using ControleDeMedicamentos.Dominio.ModuloPaciente;
+
+namespace ControleDeMedicamentos.Dominio.ModuloRequisicao
+{
+    public class Requisicao : EntidadeBase<Requisicao>
+    {
+        public Funcionario Funcionario { get; set; }
+        public Paciente Paciente { get; set; }
+        public Medicamento Medicamento { get; set; }
+        public int QuantidadeMedicamento { get; set; }
+        public DateTime DataRequisicao { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Requisicao requisicao &&
+                   Id == requisicao.Id &&
+                   EqualityComparer<Funcionario>.Default.Equals(Funcionario, requisicao.Funcionario) &&
+                   EqualityComparer<Paciente>.Default.Equals(Paciente, requisicao.Paciente) &&
+                   EqualityComparer<Medicamento>.Default.Equals(Medicamento, requisicao.Medicamento) &&
+                   QuantidadeMedicamento == requisicao.QuantidadeMedicamento &&
+                   DataRequisicao == requisicao.DataRequisicao;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Funcionario, Paciente, Medicamento, QuantidadeMedicamento, DataRequisicao);
+        }
+    }
+}
---
+++ ControleDeMedicamentos.Dominio/ModuloRequisicao/ValidadorRequisicao.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace ControleDeMedicamentos.Dominio.ModuloRequisicao
+{
+    public class ValidadorRequisicao : AbstractValidator<Requisicao>
+    {
+        public ValidadorRequisicao()
+        {
+            RuleFor(x => x.Funcionario)
+                .NotNull().WithMessage("Campo 'Funcionário' é obrigatório.");
+
+            RuleFor(x => x.Paciente)
+                .NotNull().WithMessage("Campo 'Paciente' é obrigatório.");
+
+            RuleFor(x => x.Medicamento)
+                .NotNull().WithMessage("Campo 'Medicamento' é obrigatório.");
+
+            RuleFor(x => x.QuantidadeMedicamento)
+                .NotEmpty().WithMessage("Campo 'Quantidade Medicamento' é obrigatório.")
+                .GreaterThan(0).WithMessage("Quantidade Medicamento informada é inválida.")
+                .LessThanOrEqualTo(x => x.Medicamento.QuantidadeDisponivel).WithMessage("Quantidade Medicamento não disponível.");
+
+            RuleFor(x => x.DataRequisicao)
+                .Equal(DateTime.Now.Date).WithMessage("Erro ao salvar a data da requisição.");
+        }
+    }
+}
